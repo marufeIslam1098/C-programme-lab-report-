9c7a33eedac5ed095bc529139a4cf07ce57e876d
marufeIslam1098 <marufe1098@gmail.com>
2025-11-20 23:31:28 +0600

    Create 70) C Program to Sort the Elements of an Array in Descending Order.C

---
+++ 70) C Program to Sort the Elements of an Array in Descending Order.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n; scanf("%d",&n); int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    // simple descending bubble
+    for(int i=0;i<n-1;i++)
+      for(int j=0;j<n-1-i;j++)
+        if(a[j]<a[j+1]){ int t=a[j]; a[j]=a[j+1]; a[j+1]=t; }
+    for(int i=0;i<n;i++) printf("%d ", a[i]);
+    printf("\n"); return 0;
+}
