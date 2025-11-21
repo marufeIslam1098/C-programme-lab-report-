be122396b327a185851f98a7d1c77cef8d716e22
marufeIslam1098 <marufe1098@gmail.com>
2025-11-21 23:45:03 +0600

    Create 96. C Program to Find Sum of Main Diagonal of Matrix .C

---
+++ 96. C Program to Find Sum of Main Diagonal of Matrix .C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n][n], sum = 0;
+
+    printf("Enter matrix:\n");
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            scanf("%d", &a[i][j]);
+
+    for(int i=0;i<n;i++)
+        sum += a[i][i];
+
+    printf("Main diagonal sum = %d\n", sum);
+
+    return 0;
+}
