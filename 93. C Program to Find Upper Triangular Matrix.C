54393ee9b4baf942ad2baddb434e92f94c340c7b
marufeIslam1098 <marufe1098@gmail.com>
2025-11-21 23:42:45 +0600

    Create 93. C Program to Find Upper Triangular Matrix.C

---
+++ 93. C Program to Find Upper Triangular Matrix.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    int a[n][n];
+    printf("Enter matrix:\n");
+
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("Upper Triangular:\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i > j)
+                printf("0 ");
+            else
+                printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
