4b0326fbb12b3ef051f9ed66d62caa5200e5b7f3
marufeIslam1098 <marufe1098@gmail.com>
2025-11-21 23:49:37 +0600

    Create 103. C Program to Find Smallest Element in Matrix.C

---
+++ 103. C Program to Find Smallest Element in Matrix.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter rows & columns: ");
+    scanf("%d %d",&r,&c);
+
+    int a[r][c], min;
+
+    printf("Enter matrix:\n");
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    min = a[0][0];
+
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            if(a[i][j] < min)
+                min = a[i][j];
+
+    printf("Smallest element = %d\n", min);
+
+    return 0;
+}
